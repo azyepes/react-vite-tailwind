22c89e902ec1ceb01a5edfedc614cab9d79e8c38
Luis Yps Fdez <lueryefe@gmail.com>
2024-08-27 22:20:32 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-red-100'>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
