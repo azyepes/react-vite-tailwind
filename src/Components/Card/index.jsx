232faf3af2bb204044d6bdbc6bc3cde0d949773b
Luis Yps Fdez <lueryefe@gmail.com>
2024-08-31 08:07:10 -0500

    Adding card and consuming API
    Note: Need to look for a better API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,25 @@
-const Card = () => {
-    return (
-        <div className="bg-white">
-          <figure>
-            <span></span>
-            <img src="" alt="" />
-            <div>+</div>
-          </figure>
-          <p>
-            <span>Headphone</span>
-            <span>$200</span>
-          </p>
+const Card = ( data ) => {
+  return (
+    <div className="bg-white cursor-pointer w-56 h-60">
+      <figure className="relative mb-2 w-full h-4/5">
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs  m-2 py-0.5 px-3">
+          {data.data?.category}
+        </span>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={data.data?.image}
+          alt="headphones"
+        />
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2">
+          +
         </div>
-    )
-}
+      </figure>
+      <p className="flex justify-between items-center">
+        <span className="font-light text-sm">{data.data?.name}</span>
+        <span className="font-medium text-lg">${data.data?.price}</span>
+      </p>
+    </div>
+  );
+};
 
-export { Card }
+export { Card };
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,28 @@
-import { Layaout } from "../../Components/Layout"
+import { useState, useEffect } from "react";
+import { Layaout } from "../../Components/Layout";
+import { Card } from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fake-store-api.mock.beeceptor.com/api/products")
+      .then((res) => res.json())
+      .then((data) => 
+        setItems(data)
+        // console.log(data)
+      
+        );
+  }, []);
+
   return (
-    <Layaout> 
-      home
+    <Layaout>
+      {items?.map((item) => (
+        <Card key={item.id} data={item} />
+      ))}
+      <Card />
     </Layaout>
-  )
+  );
 }
 
-export { Home }
+export { Home };
